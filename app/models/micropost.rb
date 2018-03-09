8d2e413873025b3104414e2abf246282cd4e8047
quangnk234 <nguyenkhoaquang501@gmail.com>
2018-03-09 09:01:58 +0700

    finish chapter 14 fix3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope ->{order(created_at: :desc)}
+  scope :post_feed, ->(following_ids, id){where "user_id IN (?) OR user_id = ?", following_ids, id}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: 140}
+  validates :content, presence: true, length: {maximum: Settings.micropost.max_content}
   validate :picture_size
 
   private
